da6e92e9db206c0a8edb9ab13563f19fba9c2b27
Gerd Aschemann <gerd@aschemann.net>
2019-12-13 11:50:06 +0100

    Import from Spring Boot Initializr

---
+++ src/main/java/net/aschemann/demo/spring/boot/hibernateproblem/HibernateProblemApplication.java
@@ -0,0 +1,13 @@
+package net.aschemann.demo.spring.boot.hibernateproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateProblemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/aschemann/demo/spring/boot/hibernateproblem/HibernateProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aschemann.demo.spring.boot.hibernateproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
